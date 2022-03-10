aebd0c9e6f104beb206f83adccd459a9520cfac4
Lika <85628683+lika-vorobeva@users.noreply.github.com>
2022-03-10 21:20:00 +0200

    Add plan options UI

---
+++ DVPNApp/DVPNApp/Scenes/Plans/Subviews/PlanOptionViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlanOptionViewModel.swift
+//  DVPNApp
+//
+//  Created by Lika Vorobeva on 09.03.2022.
+//
+
+import Foundation
+import SentinelWallet
+
+struct PlanOptionViewModel: Hashable {
+    let id: UInt64
+    let price: String
+    let bandwidth: String
+    let validity: String
+}
